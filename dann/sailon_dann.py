c1e7296fbad0e3ae52574c4dfa95724c74f0614c
Ajay Sridhar <ajaysridhar@berkeley.edu>
2022-01-12 14:35:11 +0530

    finish basic train and validation function

--- dann/sailon_dann.py
+++ dann/sailon_dann.py
@@ -19,7 +19,10 @@ from utils import accuracy
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
+if torch.cuda.is_available():
+    print("GPU detected")
+else:
+    print("No GPU dectected, defaulting to CPU")
 
 def custom_collate(batch):
     subject_images = []
@@ -41,7 +44,7 @@ def custom_collate(batch):
     return subject_images, verb_images, object_images, spatial_encodings, subject_labels, verb_labels, object_labels
 
 
-def train(train_loader: DataLoader, feature_extractor: Featurizer, classify_heads: List[Featurizer], optimizers: List[SGD]):
+def train(args: argparse.Namespace, train_loader: DataLoader, feature_extractor: Featurizer, classify_heads: List[Featurizer], optimizers: List[SGD]):
     subject_classifier, verb_discriminator, object_discriminator = classify_heads
     featurizer_opt, subject_opt, verb_opt, object_opt = optimizers
     
@@ -54,6 +57,7 @@ def train(train_loader: DataLoader, feature_extractor: Featurizer, classify_head
     object_losses, object_accuracies = [], []
 
     train_log = {}
+    count = 0
 
     for subject_images, verb_images, object_images, spatial_encodings, subject_labels, verb_labels, object_labels in train_loader:
         subject_images = subject_images.to(device)
@@ -77,7 +81,7 @@ def train(train_loader: DataLoader, feature_extractor: Featurizer, classify_head
         '''Training with the verb images'''
 
         features = feature_extractor(verb_images)
-        verb_preds = subject_classifier(features)
+        verb_preds = verb_discriminator(features)
         verb_loss = F.cross_entropy(verb_preds, verb_labels)
         featurizer_opt.zero_grad()
         verb_opt.zero_grad()
@@ -88,7 +92,7 @@ def train(train_loader: DataLoader, feature_extractor: Featurizer, classify_head
         '''Training with the object images'''
 
         features = feature_extractor(object_images)
-        object_preds = subject_classifier(features)
+        object_preds = object_discriminator(features)
         object_loss = F.cross_entropy(object_preds, object_labels)
         featurizer_opt.zero_grad()
         object_opt.zero_grad()
@@ -102,9 +106,22 @@ def train(train_loader: DataLoader, feature_extractor: Featurizer, classify_head
         verb_losses.append(verb_losses)
         object_losses.append(object_losses)
 
-        subject_accuracies.append(accuracy(subject_preds, subject_labels))
-        verb_accuracies.append(accuracy(verb_preds, verb_labels))
-        object_accuracies.append(accuracy(object_preds, object_labels))
+        subject_accuracy = accuracy(subject_preds, subject_labels)
+        verb_accuracy = accuracy(verb_preds, verb_labels)
+        object_accuracy = accuracy(object_preds, object_labels)
+
+        subject_accuracies.append(subject_accuracy)
+        verb_accuracies.append(verb_accuracy)
+        object_accuracies.append(object_accuracy)
+
+        if count % args.print_freq == 0:
+            print(f"Results from training batch {count}: ")
+            print(f"Subject: Loss = {subject_loss}, Accuracy = {subject_accuracy}%")
+            print(f"Verb: Loss = {verb_loss}, Accuracy = {verb_accuracy}%")
+            print(f"Object: Loss = {object_loss}, Accuracy = {object_accuracy}%")
+            print('\n')
+        
+        count += 1
     
     subject_losses = np.array(subject_losses)
     verb_losses = np.array(verb_losses)
@@ -127,9 +144,81 @@ def train(train_loader: DataLoader, feature_extractor: Featurizer, classify_head
     return train_log
 
 
-def validate():
-    # TODO: validate the model on the validation dataset
-    return 0
+def validate(args: argparse.Namespace, val_loader: DataLoader, feature_extractor: Featurizer, classify_heads: List[Featurizer]):
+    subject_classifier, verb_discriminator, object_discriminator = classify_heads
+    
+    feature_extractor.eval()
+    for head in classify_heads:
+        head.eval()
+
+    subject_losses, subject_accuracies = [], []
+    verb_losses, verb_accuracies = [], []
+    object_losses, object_accuracies = [], []
+
+    val_log = {}
+    count = 0
+
+    with torch.no_grad():
+        for subject_images, verb_images, object_images, spatial_encodings, subject_labels, verb_labels, object_labels in val_loader:
+            subject_images = subject_images.to(device)
+            verb_images = verb_images.to(device)
+            object_images = object_images.to(device)
+            subject_labels = subject_labels.to(device)
+            verb_labels = verb_labels.to(device)
+            object_labels = object_labels.to(device)
+
+            subject_features = feature_extractor(subject_images)
+            verb_features = feature_extractor(verb_images)
+            object_features = feature_extractor(object_images)
+
+            subject_preds = subject_classifier(subject_features)
+            verb_preds = verb_discriminator(verb_features)
+            object_preds = object_discriminator(object_features)
+
+            subject_loss = F.cross_entropy(subject_preds, subject_labels)
+            verb_loss = F.cross_entropy(verb_preds, verb_labels)
+            object_loss = F.cross_entropy(object_preds, object_labels)
+
+            subject_accuracy = accuracy(subject_preds, subject_labels)
+            verb_accuracy = accuracy(verb_preds, verb_labels)
+            object_accuracy = accuracy(object_preds, object_labels)
+
+            subject_losses.append(subject_loss)
+            verb_losses.append(verb_loss)
+            object_losses.append(object_loss)
+
+            subject_accuracies.append(subject_accuracy)
+            verb_accuracies.append(verb_accuracy)
+            object_accuracies.append(object_accuracy)
+
+            if count % args.print_freq == 0:
+                print(f"Results from validation batch {count}: ")
+                print(f"Subject: Loss = {subject_loss}, Accuracy = {subject_accuracy}%")
+                print(f"Verb: Loss = {verb_loss}, Accuracy = {verb_accuracy}%")
+                print(f"Object: Loss = {object_loss}, Accuracy = {object_accuracy}%")
+                print('\n')
+
+            count += 1
+
+    subject_losses = np.array(subject_losses)
+    verb_losses = np.array(verb_losses)
+    object_losses = np.array(object_losses)
+
+    subject_accuracies = np.array(subject_accuracies)
+    verb_accuracies = np.array(verb_accuracies)
+    object_accuracies = np.array(object_accuracies)
+
+    val_log.update({
+        "Subject Classification Loss (Validation Set)": subject_losses.mean(),
+        "Verb Classification Loss (Validation Set)": verb_losses.mean(),
+        "Object Classification Loss (Validation Set)": object_losses.mean(),
+
+        "Subject Classification Accuracy (Validation Set)": subject_accuracies.mean(),
+        "Verb Classification Accuracy (Validation Set)": verb_accuracies.mean(),
+        "Object Classification Accuracy (Validation Set)": object_accuracies.mean(),
+    })
+    
+    return val_log
 
 def evaluate():
     # TODO: post-training validation
@@ -207,8 +296,9 @@ def main(args):
     best_path = os.path.join(save_path, 'best')
     module_names = ['featurizer', 'subject', 'verb', 'object']
     
-
+    print("######## STARTING TRAINING LOOP #########")
     for epoch in range(args.epochs):
+        print(f"Starting epoch {epoch}")
         acc = train(train_loader, feature_extractor, classify_heads, optimizers)
 
         # save the latest module
@@ -224,36 +314,29 @@ def main(args):
                     os.path.join(latest_path, f'{module_name}.pth'),
                     os.path.join(best_path, f'{module_name}.pth'),
                 )
+    print("######## ENDING TRAINING LOOP #########\n")
+    print("######## STARTING EVALUATION #########")
 
+    print("######## ENDING EVALUATION #########\n")
     # TODO: post-training evaluation
             
-        
-        
 
-
-        
-        
-        
-
-        
-
-
-    for subject_images, verb_images, object_images, spatial_encodings, subject_labels, verb_labels, object_labels in data_loader:
-        for subject_image, verb_image, object_image, spatial_encoding, subject_label, verb_label, object_label in zip(subject_images, verb_images, object_images, spatial_encodings, subject_labels, verb_labels, object_labels):
-            if subject_image is not None:
-                print(f'Subject image shape: {subject_image.shape}')
-            if verb_image is not None:
-                print(f'Verb image shape: {verb_image.shape}')
-            if object_image is not None:
-                print(f'Object image shape: {object_image.shape}')
-            if spatial_encoding is not None:
-                print(f'Spatial encoding shape: {spatial_encoding.shape}')
-            if subject_label is not None:
-                print(f'Subject label shape: {subject_label.shape}')
-            if verb_label is not None:
-                print(f'Verb label shape: {verb_label.shape}')
-            if object_label is not None:
-                print(f'Object label shape: {object_label.shape}')
+    # for subject_images, verb_images, object_images, spatial_encodings, subject_labels, verb_labels, object_labels in data_loader:
+    #     for subject_image, verb_image, object_image, spatial_encoding, subject_label, verb_label, object_label in zip(subject_images, verb_images, object_images, spatial_encodings, subject_labels, verb_labels, object_labels):
+    #         if subject_image is not None:
+    #             print(f'Subject image shape: {subject_image.shape}')
+    #         if verb_image is not None:
+    #             print(f'Verb image shape: {verb_image.shape}')
+    #         if object_image is not None:
+    #             print(f'Object image shape: {object_image.shape}')
+    #         if spatial_encoding is not None:
+    #             print(f'Spatial encoding shape: {spatial_encoding.shape}')
+    #         if subject_label is not None:
+    #             print(f'Subject label shape: {subject_label.shape}')
+    #         if verb_label is not None:
+    #             print(f'Verb label shape: {verb_label.shape}')
+    #         if object_label is not None:
+    #             print(f'Object label shape: {object_label.shape}')
 
 
 
